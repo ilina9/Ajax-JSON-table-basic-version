d19b69428e1e3cd50ba2820ec11d06c843a1baaf
user <user@WIN-PIGSM1DK9EC>
2025-02-04 11:57:26 +0100

    Add project files.

---
+++ AjaxJSONTable1/Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using AjaxJSONTable1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AjaxJSONTable1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private static List<Person> people = new List<Person>
+        {
+            new Person { ime = "Janez", starost = 25 },
+            new Person { ime = "Micka", starost = 24 },
+            new Person { ime = "Polde", starost = 27 }
+        };
+
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            var result = new
+            {
+                st = people.Count,
+                seznam = people
+            };
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult AddData([FromBody] Person newPerson)
+        {
+            if (newPerson == null)
+            {
+                return BadRequest("Invalid data.");
+            }
+
+            people.Add(newPerson);
+
+            var result = new
+            {
+                st = people.Count,
+                seznam = people
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ AjaxJSONTable1/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace AjaxJSONTable1.Models
+{
+    public class Person
+    {
+        public string ime {get;set;}
+        public int starost {get;set;}
+    }
+}
